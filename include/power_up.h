6a263dec37867094ccfe17fb4e4bb24c1e9785ee
Jerrick Liu <jerrick.y.liu@gmail.com>
2020-12-13 11:13:53 -0500

    Transferred files over

---
+++ include/power_up.h
@@ -0,0 +1,31 @@
+#pragma once
+#include "cinder/gl/gl.h"
+#include "cinder/app/App.h"
+#include "cinder/gl/gl.h"
+#include "../../../include/glm/glm.hpp"
+
+namespace PacmanGame {
+
+class PowerUp {
+ public:
+  /**
+   * Constructs a power up given a position
+   * @param position the position of the power up
+   */
+  PowerUp(const glm::vec2 &position);
+  
+  /**
+   * Draws a power up based on its location 
+   */
+  void Draw();
+  
+  const glm::vec2 &GetPosition() const;
+
+ private:
+  glm::vec2 position_;
+  
+  const static size_t kRadius = 6;
+  
+};
+
+} // namespace PacmanGame
